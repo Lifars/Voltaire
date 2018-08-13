56dad597cf0d64e519dd10c9c4dab85cdc6eafa8
Jean-Francois Gobin <jean.gobin@lifars.com>
2018-08-13 09:04:43 +0000

    Runs more or less successfully

--- voltairedb.py
+++ voltairedb.py
@@ -6,25 +6,30 @@
 # Add comments in functions
 # Reformat code to conform to writing styles
 # Move the determination of the OS command to run outside of functions
+# Keep fixing the issues reported by pylint
+# Simplify the regex
 
 import argparse
 import os
 import re
 import sys
-from subprocess import call
 from sys import platform as _platform
-
+from subprocess import call
+import sqlite3
 
 # Global variables
 # OS we are using
 is_windows = _platform == "win32"
+
+program = os.path.abspath("vol.exe") if is_windows \
+                                     else os.path.abspath("vol.py")
 
 # Valid profiles
 valid_profiles = dict.fromkeys(
     ["VistaSP0x64", "VistaSP0x86", "VistaSP1x64", "VistaSP1x86", "VistaSP2x64",
      "VistaSP2x86", "Win10x64", "Win10x86", "Win2003SP0x86", "Win2003SP1x64",
      "Win2003SP1x86", "Win2003SP2x64", "Win2003SP2x86",
-     "Win2008R2SP0x64", "Win2008R2SP1x64", "Win2008SP1x64", "Win2008SP1x86", 
+     "Win2008R2SP0x64", "Win2008R2SP1x64", "Win2008SP1x64", "Win2008SP1x86",
      "Win2008SP2x64", "Win2008SP2x86", "Win2012R2x64", "Win2012x64",
      "Win7SP0x64", "Win7SP0x86", "Win7SP1x64", "Win7SP1x86", "Win81U1x64",
      "Win81U1x86", "Win8SP0x64", "Win8SP0x86", "Win8SP1x64", "Win8SP1x86",
@@ -73,85 +78,70 @@ valid_ip_addresses = "^([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\.([
 
 # Functions
 def individual_scan(args, command):
-    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
+    global program
     run_command(args, program, command, None)
 
 def scan(args):
+    global program
     is_valid(args)
-    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
     for command in commands:
         run_command(args, program, command, None)
 
 def scan_pid(args):
+    global program
     path = os.path.abspath(args["dest"]) + os.sep
     logfile = "{path}ps_dump.txt".format(path=path)
-    program = os.path.abspath("vol.exe") if is_windows else "vol.py"
     for command in commands_with_processes:
         if not os.path.exists(path + command):
             os.makedirs(path + command)
-        with open(logfile, encoding='utf-8') as f:
-            for line in f:
+        with open(logfile) as logf:        # Suppressed encoding='utf-8'
+            for line in logf:
                 word = line.split()
                 run_command(args, program, command, word[0])
 
 def dump_pid(args):
     path = os.path.abspath(args["dest"]) + os.sep
-    fullPath = "{path}ES{number}_{command}.txt"
-    ps_files = [fullPath.format(path=path, number=args["es"], command="pslist"),
-                fullPath.format(path=path, number=args["es"], command="psscan"),
-                fullPath.format(path=path, number=args["es"], command="pstree"),
-                fullPath.format(path=path, number=args["es"], command="psxview")]
+    fullpath = "{path}ES{number}.db".format(path=path,
+                                            number=args["es"])
     ps_dump = "{path}ps_dump.txt".format(path=path)
-    ps_dump_file = open(ps_dump, "w", encoding='utf-8')
+    ps_dump_file = open(ps_dump, "w")   # Suppressed encoding='utf-8'
     pid_dict = {}
-    for file in ps_files:
-        pid_dict.update(get_process(file))
+    pid_dict.update(get_process(fullpath))
     for key in sorted(pid_dict, key=float):
         ps_dump_file.write(key + "\n")
 
-def get_process(file):
+def get_process(fullpath):
+    """ Returns a dictionary with all the PIDs found by PSList, PSScan,
+        PSTree and PsXview.
+    """
     temp_dict = {}
-    with open(file, encoding='utf-8') as f:
-        headers = f.readline()
-        dashes = f.readline()
-        rightPID1 = headers.find("PID")
-        PID1 = rightPID1 - dashes[rightPID1:0:-1].find(" ")
-        rightPID2 = headers.find("PID", rightPID1 + 1)
-        PID2 = rightPID2 - dashes[rightPID2:0:-1].find(" ")
-        try:
-            next(f)
-        except Exception:
-            pass
-        for line in f:
-            try:
-                if rightPID2 != -1:
-                    process1 = line[PID1:].split()[0]
-                    process2 = line[PID2:].split()[0]
-
-                    if process1.isdigit():
-                        temp_dict[process1] = None
-                    if process2.isdigit():
-                        temp_dict[process2] = None
-                else:
-                    process1 = line[PID1:].split()[0]
-
-                    if process1.isdigit():
-                        temp_dict[process1] = None
-            except Exception:
-                pass
-
+    dbconn = sqlite3.connect(fullPath)
+    dbcursor = dbconn.cursor()
+    queries = [("PID", "PSList"),
+               ("PID", "PSScan"),
+               ("Pid", "PSTree"),
+               ("PID", "PsXview")]
+    for qfield, qtable in queries:
+        query = "SELECT {field} from {table}".format(field=qfield,
+                                                     table=qtable)
+        print query
+        for row in dbcursor.execute(query):
+            for item in row:
+                temp_dict[item] = None
+    dbconn.close()
     return temp_dict
 
 def filter_mutantscan(args):
     path = os.path.abspath(args["dest"]) + os.sep
-    logfile = "{path}ES{number}_{command}.txt".format(path=path, number=args["es"],
-                                                      command="mutantscan -s")
+    logfile = "{path}ES{number}_{command}.txt".format(path=path,
+                                                      number=args["es"],
+                                                      command="mutantscan-s")
     outfile = "{path}mutantscan_filter.txt".format(path=args["dest"] + os.sep)
-    open_outfile = open(outfile, "w", encoding='utf-8')
+    open_outfile = open(outfile, "w")       # Suppressed encoding='utf-8'
 
     nameMap = {}
 
-    with open(logfile, encoding='utf-8') as f:
+    with open(logfile) as f:                # Suppressed encoding='utf-8'
         add_line_flag = 0
         open_outfile.write(next(f))
         open_outfile.write(next(f))
@@ -160,7 +150,7 @@ def filter_mutantscan(args):
                 if not re.search("0x\w{16}|\.\.\.", word) and not word.isdigit():
                     add_line_flag = 1
 
-            if (add_line_flag == 1):
+            if add_line_flag == 1:
                 nameMap[word] = line
                 add_line_flag = 0
 
@@ -173,9 +163,9 @@ def filter_netscan(args):
                                                       command="netscan")
 
     outfile = "{path}netscan_filter.txt".format(path=args["dest"] + os.sep)
-    open_outfile = open(outfile, "w", encoding='utf-8')
+    open_outfile = open(outfile, "w")       # Suppressed encoding='utf-8'
 
-    with open(logfile, encoding='utf-8') as f:
+    with open(logfile) as f:                # Suppressed encoding='utf-8'
         open_outfile.write(next(f))
         for line in f:
             if is_in_range(line):
@@ -198,37 +188,36 @@ def is_valid(args):
     args["dest"] = os.path.abspath(args["dest"])
 
     if "src" in args:
-        print("Source file: {src}".format(src=args["src"]))
+        print "Source file: {src}".format(src=args["src"])
 
     if "dest" in args:
         if not os.path.exists(args["dest"]):
             os.makedirs(args["dest"])
 
-        print("Destination directory: {dest}".format(dest=args["dest"]))
+        print "Destination directory: {dest}".format(dest=args["dest"])
 
     if "profile" in args:
         if args["profile"] in valid_profiles:
-            print("Profile name: {profile}".format(profile=args["profile"]))
+            print "Profile name: {profile}".format(profile=args["profile"])
         else:
-            print("Profile not valid: {profile}".format(profile=args["profile"]))
+            print "Profile not valid: {profile}".format(profile=args["profile"])
             sys.exit(1)
     else:
-        print("WARNING: No profile set!")
-
+        print "WARNING: No profile set!"
     if "es" in args:
-        print("ES: {es}".format(es=args["es"]))
+        print "ES: {es}".format(es=args["es"])
     else:
-        print("NOTICE: No ES set. Defaulting to ES=1.")
+        print "NOTICE: No ES set. Defaulting to ES=1."
 
 def run_command(args, program, command, pid):
-#    path = args["dest"] + os.sep
-    outfile = "{path}ES{number}_{command}.db".format(path=path, number=args["es"], command=command)
+    path = args["dest"] + os.sep
+    outfile = "{path}ES{number}.db".format(path=path, number=args["es"], command=command)
     outflag = "--output=sqlite --output-file="
     command_with_flag = command
 
     if command in commands_with_processes:
-        outfile = "{path}{dir}ES{number}_{command}_{pid}.txt".format(path=path, dir=command + os.sep, number=args["es"],
-                                                                     command=command, pid=pid)
+        outfile = "{path}ES{number}.db".format(path=path,
+                                               number=args["es"])
         if command in dump_commands:
             outflag = "--dump-dir="
             outfile = "{path}{command}".format(path=path, command=command)
@@ -238,25 +227,29 @@ def run_command(args, program, command, pid):
                     os.makedirs(outfile)
         command_with_flag = "{command} -p {pid}".format(command=command, pid=pid)
     if "profile" in args:
-        params = "-f {src} --profile={profile} {command} {destflag}\"{dest}\"".format(src=args["src"],
-                                                                                      profile=args["profile"],
-                                                                                      command=command_with_flag,
-                                                                                      destflag=outflag,
-                                                                                      dest=outfile)
+        params = "-f {src} --profile={profile} {command} {destflag}\"{dest}\""
+        params = params.format(src=args["src"],
+                               profile=args["profile"],
+                               command=command_with_flag,
+                               destflag=outflag,
+                               dest=outfile)
     else:
-        params = "-f {src} {command} {destflag}\"{dest}\"".format(src=args["src"], command=command_with_flag,
-                                                                  destflag=outflag,
-                                                                  dest=outfile)
-    print("{program} {params}".format(program=program, params=params))
-    result = call("{program} {params}".format(program=program, params=params))
+        params = "-f {src} {command} {destflag}\"{dest}\""
+        params = params.format(src=args["src"],
+                               command=command_with_flag,
+                               destflag=outflag,
+                               dest=outfile)
+    print "{program} {params}".format(program=program, params=params)
+    result = call("{program} {params}".format(program=program, params=params),
+                  shell=True)
     if result == 0:
-        print("Completed {command}".format(command=command_with_flag))
+        print "Completed {command}".format(command=command_with_flag)
     else:
-        print("Error running {command}".format(command=command_with_flag))
+        print "Error running {command}".format(command=command_with_flag)
     if is_windows:
         path = args["dest"] + os.sep
         outfile = "\"{outfile}\"".format(outfile="{path}ES{number}_autorun.txt".format(path=path, number=args["es"]))
-        print("Starting {command}".format(command=command))
+        print "Starting {command}".format(command=command)
 
         if re.search("procdump", outfile) or re.search("dlldump", outfile):
             outflag = "--dump-dir="
@@ -272,50 +265,51 @@ def run_command(args, program, command, pid):
             params = "-f {src} printkey \"Software\\Microsoft\\Windows\\CurrentVersion\\Run\" {destflag}{dest}".format(
                 src=args["src"], destflag=outflag, dest=outfile)
 
-        result = call("{program} {params}".format(program=program, params=params))
+        result = call("{program} {params}".format(program=program, params=params),
+                      shell=True)
 
         if result == 0:
-            print("Completed autorun")
+            print "Completed autorun"
         else:
-            print("Error running autorun \n " + params)
+            print "Error running autorun \n " + params
 
-    print("Volatility files saved to {dest}".format(dest=args["dest"]))
-
+    print "Volatility files saved to {dest}".format(dest=args["dest"])
 
 def process(args):
     args["src"] = os.path.abspath(args["src"])
     args["dest"] = os.path.abspath(args["dest"])
 
-
+# Main
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Batches common Volatility commands")
     sub_parsers = parser.add_subparsers(dest="subparser_name")  # this line changed
-
     scan_parser = sub_parsers.add_parser('scan')
     scan_parser.set_defaults(which="scan")
-
-    scan_parser.add_argument("-s", "--src", help="Input file", required=True)
-    scan_parser.add_argument("-d", "--dest", help="Output directory", required=False, default="voltaire")
-    scan_parser.add_argument("-p", "--profile", help="Profile name", required=False)
-    scan_parser.add_argument("-e", "--es", help="ES mode", default=1, required=False)
-
+    scan_parser.add_argument("-s", "--src",
+                             help="Input file", required=True)
+    scan_parser.add_argument("-d", "--dest",
+                             help="Output directory", required=False,
+                             default="voltaire")
+    scan_parser.add_argument("-p", "--profile",
+                             help="Profile name", required=False)
+    scan_parser.add_argument("-e", "--es",
+                             help="ES mode", default=1, required=False)
     process_parser = sub_parsers.add_parser("process")
     process_parser.set_defaults(which="process")
-
-    process_parser.add_argument("-s", "--src", help="Input directory", required=True)
-    process_parser.add_argument("-d", "--dest", help="Output directory", required=False, default="voltaire")
-
+    process_parser.add_argument("-s", "--src",
+                                help="Input directory", required=True)
+    process_parser.add_argument("-d", "--dest",
+                                help="Output directory",
+                                required=False, default="voltaire")
     args = vars(parser.parse_args())
-
     subcommand = args.get("which", "")
-
-    if "scan" == subcommand:
+    if subcommand == "scan":
         scan(args)
         dump_pid(args)
         scan_pid(args)
         filter_mutantscan(args)
         filter_netscan(args)
-    elif "process" == subcommand:
+    elif subcommand == "process":
         process(args)
     else:
         parser.print_help()
