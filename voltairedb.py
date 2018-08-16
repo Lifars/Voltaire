3b168b4b888e41727dccee00e57abe4b59a376b3
Jean-Francois Gobin <jean.gobin@lifars.com>
2018-08-16 05:29:40 +0000

    Removed code for commands that dump files or do not log to database

--- voltairedb.py
+++ voltairedb.py
@@ -4,7 +4,6 @@
 
 import argparse
 import os
-import re
 import sys
 from sys import platform as _platform
 from subprocess import call
@@ -40,62 +39,9 @@ COMMANDS = ["amcache", "apihooks", "atoms", "atomscan", "bigpools", "bioskbd",
             "truecryptpassphrase", "truecryptsummary", "unloadedmodules",
             "windows", "wintree", "connections", "userassist"]
 
-COMMANDS_WITH_PROCESSES = [
-    "apihooks", "dlldump", "malfind", "procdump"
-]
-
-DUMP_COMMANDS = [
-    "dlldump", "procdump", "dumpfiles"
-]
-
 NON_DB_COMS = ["dumpregistry", "filescan", "iehistory", "screenshot",
                "truecryptmaster", "truecryptpassphrase",
                "truecryptsummary", "windows", "wintree", "userassist"]
-
-# Regex string of public ip addresses that are excluded
-PUBLIC_IP_ADDRESSES_TO_EXCLUDE = '|'.join([
-    "(^0\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^10\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^100\.(6[4-9]|[7-9][0-9]|1([0-1][0-9]|2[0-7]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^127\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^169\.254\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^172\.(1[6-9]|2[0-9]|3[0-1])\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^192\.0\.0\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^192\.0\.2\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^192\.88\.99\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^192\.168\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^198\.(1[8-9])\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^198\.51\.100\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^203\.0\.113\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^(2(2[4-9]|3[0-9]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))",
-    "(^(2(4[0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))\." + \
-    "([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5])))"])
-
-# Regex to see if a string is an ip address
-VALID_IP_ADDRESSES = \
-                   "^([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))" + \
-                   "\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))" + \
-                   "\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))" + \
-                   "\.([0-9]|[1-9][0-9]|1([0-9][0-9])|2([0-4][0-9]|5[0-5]))"
 
 # Functions
 def individual_scan(comargs, command):
@@ -107,29 +53,6 @@ def scan(comargs):
     is_valid(comargs)
     for command in COMMANDS:
         run_command(comargs, PROGRAM, command, None)
-
-def scan_pid(comargs):
-    global PROGRAM
-    path = os.path.abspath(comargs["dest"]) + os.sep
-    logfile = "{path}ps_dump.txt".format(path=path)
-    for command in COMMANDS_WITH_PROCESSES:
-        if not os.path.exists(path + command):
-            os.makedirs(path + command)
-        with open(logfile) as logf:        # Suppressed encoding='utf-8'
-            for line in logf:
-                word = line.split()
-                run_command(comargs, PROGRAM, command, word[0])
-
-def dump_pid(args):
-    path = os.path.abspath(args["dest"]) + os.sep
-    fullpath = "{path}ES{number}.db".format(path=path,
-                                            number=args["es"])
-    ps_dump = "{path}ps_dump.txt".format(path=path)
-    ps_dump_file = open(ps_dump, "w")   # Suppressed encoding='utf-8'
-    pid_dict = {}
-    pid_dict.update(get_process(fullpath))
-    for key in sorted(pid_dict, key=float):
-        ps_dump_file.write(key + "\n")
 
 def get_process(fullpath):
     """ Returns a dictionary with all the PIDs found by PSList, PSScan,
@@ -151,56 +74,6 @@ def get_process(fullpath):
                 temp_dict[item] = None
     dbconn.close()
     return temp_dict
-
-def filter_mutantscan(args):
-    path = os.path.abspath(args["dest"]) + os.sep
-    logfile = "{path}ES{number}_{command}.txt".format(path=path,
-                                                      number=args["es"],
-                                                      command="mutantscan-s")
-    outfile = "{path}mutantscan_filter.txt".format(path=args["dest"] + os.sep)
-    open_outfile = open(outfile, "w")       # Suppressed encoding='utf-8'
-
-    name_map = {}
-
-    with open(logfile) as f:                # Suppressed encoding='utf-8'
-        add_line_flag = 0
-        open_outfile.write(next(f))
-        open_outfile.write(next(f))
-        for line in f:
-            for word in line.split():
-                if not re.search("0x\w{16}|\.\.\.", word) and \
-                   not word.isdigit():
-                    add_line_flag = 1
-
-            if add_line_flag == 1:
-                name_map[word] = line
-                add_line_flag = 0
-
-        for key in sorted(name_map, key=lambda v: v.upper()):
-            open_outfile.write(name_map[key])
-
-def filter_netscan(args):
-    path = os.path.abspath(args["dest"]) + os.sep
-    logfile = "{path}ES{number}_{command}.txt".format(path=path,
-                                                      number=args["es"],
-                                                      command="netscan")
-    outfile = "{path}netscan_filter.txt".format(path=args["dest"] + os.sep)
-    open_outfile = open(outfile, "w")       # Suppressed encoding='utf-8'
-
-    with open(logfile) as f:                # Suppressed encoding='utf-8'
-        open_outfile.write(next(f))
-        for line in f:
-            if is_in_range(line):
-                open_outfile.write(line)
-
-def is_in_range(line):
-    add_line_flag = 0
-    for word in line.split():
-        if re.search(VALID_IP_ADDRESSES, word) and \
-           not re.search(PUBLIC_IP_ADDRESSES_TO_EXCLUDE, word):
-            add_line_flag = 1
-            break
-    return add_line_flag == 1
 
 def is_valid(args):
     args["src"] = "\"{path}\"".format(path=os.path.abspath(args["src"]))
@@ -227,10 +100,8 @@ def is_valid(args):
 def run_command(args, executable, command, pid):
     path = args["dest"] + os.sep
     if command in NON_DB_COMS:
-        outfile = "{path}ES{number}-{command}.txt".format(path=path,
-                                                          number=args["es"],
-                                                          command=command)
-        outflag = "--output=text --output-file="
+        # Only log to DB. Everything else is ignored.
+        return
     else:
         outfile = "{path}ES{number}.db".format(path=path,
                                                number=args["es"],
@@ -238,19 +109,6 @@ def run_command(args, executable, command, pid):
         outflag = "--output=sqlite --output-file="
     command_with_flag = command
     outlog = open(args["log"], "at")
-    if command in COMMANDS_WITH_PROCESSES:
-        outfile = "{path}ES{number}.db".format(path=path,
-                                               number=args["es"])
-        if command in DUMP_COMMANDS:
-            outflag = "--dump-dir="
-            outfile = "{path}{command}".format(path=path, command=command)
-            if re.search("dlldump", command):
-                outfile += "{command}_{pid}".format(command=os.sep + command,
-                                                    pid=pid)
-                if not os.path.exists(outfile):
-                    os.makedirs(outfile)
-        command_with_flag = "{command} -p {pid}".format(command=command,
-                                                        pid=pid)
     if "profile" in args:
         params = "-f {src} --profile={profile} {command} {destflag}\"{dest}\""
         params = params.format(src=args["src"],
@@ -345,11 +203,7 @@ if __name__ == "__main__":
     subcommand = args.get("which", "")
     if subcommand == "scan":
         scan(args)
-        dump_pid(args)
-        scan_pid(args)
         export_autorun(args)
-        #filter_mutantscan(args)
-        #filter_netscan(args)
     elif subcommand == "process":
         process(args)
     else:
